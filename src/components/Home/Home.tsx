b8eb9bacae9af949f3379ce3e7be273ab9f9f165
j0n9hyun <j0n9hyun@kakao.com>
2021-05-26 14:25:54 +0900

    Initialize

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <header>Header</header>
+      <section className='section first'>section1</section>
+      <section className='section second'>section2</section>
+      <section className='section third'>section3</section>
+      <section className='section fourth'>section4</section>
+      <footer>footer</footer>
+    </>
+  );
+};
+
+export default Home;
