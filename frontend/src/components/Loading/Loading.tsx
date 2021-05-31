c548ce1faf2316f1b884bbe9cc03ea358312e29d
j0n9hyun <j0n9hyun@kakao.com>
2021-05-31 16:24:15 +0900

    Add FontAwesome & Modify Loading Icon

--- frontend/src/components/Loading/Loading.tsx
+++ frontend/src/components/Loading/Loading.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import '../../static/scss/home.scss';
 const Loading = () => {
-  return <div>로딩중</div>;
+  return (
+    <div className='loading-wrapper'>
+      <FontAwesomeIcon icon={faSpinner} spin size='4x' />;
+    </div>
+  );
 };
 
 export default Loading;
